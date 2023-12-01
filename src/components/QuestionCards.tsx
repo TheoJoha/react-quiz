3007a5b87c27b4017d84ff4b2ca58359cea939a1
Theodor Johansson <t.j.u@telia.com>
2023-12-01 23:10:46 +0100

    components

---
+++ src/components/QuestionCards.tsx
@@ -0,0 +1,36 @@
+import react from "react";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => (
+<div>
+    <p className="number">
+    Question: {questionNumber} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question}} />
+    <div>
+        {answers.map(answer => {
+            <div>
+                <button disabled={userAnsert} >
+                    <span dangerouslySetInnerHTML={{ __html: question}} />
+                </button>
+            </div>
+        })}
+    </div>
+</div>);
+
+export default QuestionCard;
